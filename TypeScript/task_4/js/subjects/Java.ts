b14d1fe00df6b4e479aa9f19b75c543b0addf835
Bengin05 <uzunbengin9@gmail.com>
2026-05-20 10:56:45 +0000

    implement Teacher interface and Cpp/React/Java subjects

---
+++ TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,20 @@
+namespace Subjects {
+    export interface Teacher {
+        experienceTeachingJava?: number;
+    }
+}
+
+namespace Subjects {
+    export class Java extends Subject {
+        getRequirements() {
+            return 'Here is the list of requirements for Java';
+        }
+
+        getAvailableTeacher() {
+            if(this.teacher.experienceTeachingReact) {
+                return `Available Teacher: ${this.teacher.firstName}`;
+            }
+            return 'No available teacher';
+        }
+    }
+}
